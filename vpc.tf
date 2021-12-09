f98904a14bec6bd42592a9eb665732c832f56142
PARKHARAM <gkfka133@naver.com>
2021-12-09 16:26:14 +0900

    commit modify

--- vpc.tf
+++ vpc.tf
@@ -9,9 +9,8 @@ variable "region" {
 provider "google" {
   project = var.project_id
   region  = var.region
-  
   credentials = file("/var/lib/jenkins/pjt-an3-gketest-dev-78cdd3d69f38.json")
-
+  #credentials = file("C:/key/pjt-an3-gketest-dev-78cdd3d69f38.json")
 }
 
 data "google_compute_subnetwork" "subnet" {
