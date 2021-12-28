e0eb035c6f38e929a0a63906d9fb34d883d64dff
PARKHARAM <gkfka133@naver.com>
2021-12-28 13:50:59 +0900

    test

--- gke.tf
+++ gke.tf
@@ -20,7 +20,7 @@ variable "gke_num_nodes" {
 resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
   location = var.region
-  
+ 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
@@ -61,6 +61,7 @@ resource "google_container_node_pool" "primary_nodes" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.full_control"
     ]
 
     labels = {
