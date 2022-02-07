ccc073c8677a09ccdde6a7fb9740ab42d4abf534
PARKHARAM <gkfka133@naver.com>
2022-02-07 13:42:12 +0900

    first

--- gke.tf
+++ gke.tf
@@ -70,7 +70,7 @@ resource "google_container_node_pool" "primary_nodes" {
 
     # preemptible  = true
     #machine_type = "n1-standard-1"
-    machine_type = "e2-standard-4"
+    machine_type = "e2-standard-2"
     tags         = ["gke-node", "${var.project_id}-gke", "sgtag-80", "sgtag-4567"]
     metadata = {
       disable-legacy-endpoints = "true"
